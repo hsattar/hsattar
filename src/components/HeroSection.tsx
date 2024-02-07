1517311413fe6c3ab12f3cfbec6ccfc4d8779221
Hasan Sattar <hasanraza1798@gmail.com>
2024-02-07 20:16:20 +0000

    added hero section

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,5 @@
+export default function HeroSection() {
+  return (
+    <h2 className="text-3xl text-center font-semibold mt-20">I'm <span className="text-indigo-600">Hasan</span>. A Full Stack Developer.</h2>
+  )
+}

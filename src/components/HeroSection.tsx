a94172a091c35fb541720712b38ff10012019c1c
Hasan Sattar <hasanraza1798@gmail.com>
2024-02-08 18:55:13 +0000

    updated hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,9 +2,25 @@ import Image from "next/image";
 
 export default function HeroSection() {
   return (
-    <div className="flex flex-col justify-center items-center">
-    <h2 className="text-3xl text-center font-semibold mt-20">I'm <span className="text-indigo-600">Hasan</span>. A Full Stack Developer.</h2>
-    <Image src="/assets/hasan.png" className="mt-12" alt="Hasan Sattar" width="400" height="800" />
-    </div>
+    // <div className="flex flex-col justify-center items-center">
+    //   <h2 className="text-3xl text-center font-semibold mt-20">I'm <span className="text-indigo-600">Hasan</span>. A Full Stack Developer.</h2>
+    //   <Image src="/assets/hasan.png" className="mt-12" alt="Hasan Sattar" width="400" height="800" />
+    // </div>
+    <section className="flex flex-col lg:flex-row items-center my-4 select-none">
+    
+          <div className="flex-grow flex flex-col items-center justify-center">
+              <h1 className="text-7xl p-4 font-semibold text-center">Hasan Sattar</h1>
+              <p className="text-3xl my-4 text-center">Full Stack Developer</p>
+              <div className="social-icons text-4xl mb-8 lg:mb-0">
+                  <a className="mx-3" href="https://github.com/hsattar" target="_blank" rel="noreferrer"><i className="bi bi-github"></i></a>
+                  <a className="mx-3" href="https://www.linkedin.com/in/hasanrsattar/" target="_blank" rel="noreferrer"><i className="bi bi-linkedin"></i></a>
+              </div>
+          </div>
+
+          <div className="md:w-[35vw] flex justify-center">
+              <img className="h-[50vh] rounded-lg" src="/assets/hasan.jpg" alt="Hasan Sattar" />
+          </div>
+
+    </section>
   )
 }

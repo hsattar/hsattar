b5d521476d8c12552358e47baea8284284a63dd3
Hasan Sattar <hasanraza1798@gmail.com>
2024-02-08 19:40:19 +0000

    added gradient colour to name and social links

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,4 +1,6 @@
-import Image from "next/image";
+import Image from "next/image"
+import GitHubIcon from '@mui/icons-material/GitHub'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
 
 export default function HeroSection() {
   return (
@@ -9,11 +11,11 @@ export default function HeroSection() {
     <section className="flex flex-col lg:flex-row items-center my-4 select-none">
     
           <div className="flex-grow flex flex-col items-center justify-center">
-              <h1 className="text-7xl p-4 font-semibold text-center">Hasan Sattar</h1>
+              <h1 className="text-7xl p-4 font-semibold text-center gradient-text">Hasan Sattar</h1>
               <p className="text-3xl my-4 text-center">Full Stack Developer</p>
-              <div className="social-icons text-4xl mb-8 lg:mb-0">
-                  <a className="mx-3" href="https://github.com/hsattar" target="_blank" rel="noreferrer"><i className="bi bi-github"></i></a>
-                  <a className="mx-3" href="https://www.linkedin.com/in/hasanrsattar/" target="_blank" rel="noreferrer"><i className="bi bi-linkedin"></i></a>
+              <div className="text-4xl mb-8 lg:mb-0">
+                  <a className="mx-3" href="https://github.com/hsattar" target="_blank" rel="noreferrer"><GitHubIcon fontSize="large" /></a>
+                  <a className="mx-3" href="https://www.linkedin.com/in/hasanrsattar/" target="_blank" rel="noreferrer"><LinkedInIcon fontSize="large" /></a>
               </div>
           </div>
 

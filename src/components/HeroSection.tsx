fd9b955d43f105913b97201081bd24df80c8afb9
Hasan Sattar <hasanraza1798@gmail.com>
2024-05-02 00:10:06 +0100

    added tech skills with fade animation

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,13 +3,15 @@
 import GitHubIcon from '@mui/icons-material/GitHub'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import { motion } from 'framer-motion'
+import { SiAmazonaws, SiBootstrap, SiCss3, SiExpress, SiFramer, SiHtml5, SiJavascript, SiJest, SiMongodb, SiNextdotjs, SiNodedotjs, SiPostgresql, SiPython, SiReact, SiRedux, SiTailwindcss, SiTypescript, SiVercel } from "react-icons/si"
 
 export default function HeroSection() {
   return (
+    <>
     <section className="flex flex-col lg:flex-row items-center my-4 select-none min-h-screen">
       <div className="flex-grow flex flex-col items-center justify-center">
         <motion.div
-          variants={fadeInAnimationVariants}
+          variants={slideUpAnimationVariants}
           initial="initial"
           whileInView="animate"
           viewport={{ once: true }}
@@ -19,7 +21,7 @@ export default function HeroSection() {
         </motion.div>
         <div className="flex text-4xl my-8 lg:mb-0">
           <motion.div
-            variants={fadeInAnimationSocialVariants}
+            variants={bounceInAnimationVariants}
             initial="initial"
             whileInView="animate"
             viewport={{ once: true }}
@@ -30,7 +32,7 @@ export default function HeroSection() {
             </a>
           </motion.div>
           <motion.div
-            variants={fadeInAnimationSocialVariants}
+            variants={bounceInAnimationVariants}
             initial="initial"
             whileInView="animate"
             viewport={{ once: true }}
@@ -41,6 +43,34 @@ export default function HeroSection() {
             </a>
           </motion.div>
         </div>
+        <motion.div 
+          variants={fadeInAnimationVariants}
+          initial="hidden"
+          animate="visible"
+          exit="remove"
+          whileInView="animate"
+          viewport={{ once: true }}
+          className='grid grid-cols-6 gap-16 mt-20'
+        >
+          <SiHtml5 size={75} />
+          <SiCss3 size={75} />
+          <SiBootstrap size={75} />
+          <SiTailwindcss size={75} />
+          <SiFramer size={75} />
+          <SiJavascript size={75} />
+          <SiTypescript size={75} />
+          <SiReact size={75} />
+          <SiRedux size={75} />
+          <SiNextdotjs size={75} />
+          <SiPython size={75} />
+          <SiNodedotjs size={75} />
+          <SiExpress size={75} />
+          <SiMongodb size={75} />
+          <SiPostgresql size={75} />
+          <SiJest size={75} />
+          <SiVercel size={75} />
+          <SiAmazonaws size={75} />
+        </motion.div>
         {/* <div className='flex mt-16'>
           <a className=''>Tech Stack</a>
           <a>Projects</a>
@@ -48,10 +78,11 @@ export default function HeroSection() {
         </div> */}
       </div>
     </section>
+    </>
   )
 }
 
-const fadeInAnimationVariants = {
+const slideUpAnimationVariants = {
   initial: {
       opacity: 0,
       y: 100
@@ -60,13 +91,12 @@ const fadeInAnimationVariants = {
       opacity: 1,
       y: 0,
       transition: {
-          delay: 0.25,
           type: 'spring'
       }
   })
 }
 
-const fadeInAnimationSocialVariants = {
+const bounceInAnimationVariants = {
   initial: {
       opacity: 0,
       y: 100
@@ -79,4 +109,16 @@ const fadeInAnimationSocialVariants = {
           type: 'spring'
       }
   })
+}
+
+const fadeInAnimationVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 1.25,
+      duration: 2.5
+    }
+  },
+  remove: { opacity: 0 }
 }

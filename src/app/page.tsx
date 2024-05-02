4326bf220dd64f418ed616511dcc22d5170fc2c7
Hasan Sattar <hasanraza1798@gmail.com>
2024-05-02 14:53:17 +0100

    replaced projects section with new component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import HeroSection from '@/components/HeroSection'
 import Experiences from '@/components/Experiences'
-import Projects from '@/components/Projects'
+import CodingProjects from '@/components/CodingProjects'
 
 export default function Home() {
   return (
     <>
       <HeroSection />
-      <Projects />
+      <CodingProjects />
       <Experiences />
     </>
   )
